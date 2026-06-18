0ed28360f229965f8bbdd550a6d854d0630e9975
saurabh yadav <saurabhyadav@saurabhs-MacBook-Air.local>
2026-06-18 11:14:46 +0530

    fix upload video

--- backend/app/api/routers/videos.py
+++ backend/app/api/routers/videos.py
@@ -138,8 +138,8 @@ async def upload_video(
     if category is None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid category.")
     video_path = await upload_video(video)
-thumbnail_path = await upload_image(thumbnail)
-created = create_video(
+    thumbnail_path = await upload_image(thumbnail)
+    created = create_video(
         db,
         owner=current_user,
         title=title,
