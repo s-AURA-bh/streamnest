35f42655e60c66eadd85dfa636e36814221a8e5a
saurabh yadav <saurabhyadav@saurabhs-MacBook-Air.local>
2026-06-18 11:05:17 +0530

    fix indentation

--- backend/app/api/routers/videos.py
+++ backend/app/api/routers/videos.py
@@ -139,7 +139,7 @@ async def upload_video(
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid category.")
     video_path = await upload_video(video)
 thumbnail_path = await upload_image(thumbnail)
-    created = create_video(
+created = create_video(
         db,
         owner=current_user,
         title=title,
